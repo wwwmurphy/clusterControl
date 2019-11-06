3ba62ed844832c3f2d83ee685e120fb3699a9876
wwwmurphy <walter.murphy@gmail.com>
2019-11-05 19:51:51 -0800

    trying to get login/out to work

--- netqueue.py
+++ netqueue.py
@@ -1,50 +1,46 @@
-import re, socket, sys, select, time
+import select, sys
 from paramiko.py3compat import u
 from Queue import Queue
-from Queue import Empty
+
+def puts(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
 
 class Netqueue:
-    def __init__(self, chan, pattern=None):
+    def __init__(self, chan, tmo):
         self.q = Queue()
         self.chan = chan
-        #nlre = re.compile(r'\W+$')
-        #nlres = nlre.match("")
+        self.tmo = tmo
+        self.chan.settimeout(tmo)
 
-    def net2q(self, tmo):
-        self.chan.settimeout(0.0)
-        bufr = ' '
-        lines = []
-        lastLine = ''
-        now = time.time()
-        while True:
-            r, w, e = select.select([self.chan], [], [], 1.0)
-            if self.chan in r:
-                try:
-                    x = u(self.chan.recv(1024))
-                    if len(x) == 0:
+    def net2q(self):
+        ch = self.chan
+        chunk = ''
+
+        r, w, e = select.select([ch], [], [], self.tmo)
+        if not any([any(r),any(w),any(e)]):
+            self.q.put('T' + chunk)  # Timeout
+            return 'T'
+        if ch in r:
+            if ch.recv_ready():   # caught in loop here. recv never gets ready. unterm line ??
+                s = ch.recv(len(ch.in_buffer))
+                chunk += s.decode('ascii',errors='ignore').encode('ascii')
+                while True:
+                    if '\n' in chunk:
+                        x = chunk.index('\n')
+                        self.q.put('L' + chunk[0:x])  # Line
+                        chunk = chunk[x+1:]
+                    else:
                         break
-                except socket.timeout:
-                    pass
-
-                bufr = lastLine + str(x)
-                lines = [ x for x in bufr.splitlines() if len(x) != 0 ]
-                if ( x.endswith('/n') or x.endswith('/r') ):
-                    lastLine = ''
-                else:
-                    if len(lines) > 0:
-                        lastLine = lines[-1]
-                        lines = lines[0:-1]
-                        lastLine = '' if len(lastLine) is 0 else lastLine + '\n'
-   
-                for line in lines:
-                    self.q.put(line)
-
-            if time.time() > now + tmo:
-                break
-        return
+            if ch.recv_stderr_ready():
+                stderr.channel.recv_stderr(len(ch.in_stderr_buffer))
+        return 'L'
 
     def send2net(self, x):
         self.chan.send(x)
+
+    def getQ(self):
+        return self.q
 
     def empty(self):
         return self.q.empty()
@@ -54,7 +50,7 @@ class Netqueue:
 
     def getFromQ(self):
         try:
-            return self.q.get_nowait()
+            return self.q.get()
         except:
             return None
 
